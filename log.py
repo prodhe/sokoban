54b5e57c601c00150f0e15225faddec4436b8be4
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-02-21 15:45:36 +0100

    removed debugging printing

--- log.py
+++ log.py
@@ -15,13 +15,9 @@ class Log(object):
 
     def write(self, msg):
         stack = traceback.extract_stack()
-        print "%r" % stack
         try:
             call = stack.pop(-3)
         except IndexError:
             call = stack.pop(-2)
-        for field in call:
-            print "%r" % (field),
-        print "\n" + "-" * 10
         s = "%s:%d: in %s : %s: %s" % (call[0], call[1], call[2], call[3], msg)
         self.log.append(s)
